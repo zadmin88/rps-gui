97bb1c04cf730e395c9e1f632548ef180776686d
Simon Franco <zaimon88@gmail.com>
2022-11-16 13:58:43 -0500

    Add GUI

--- javascript.js
+++ javascript.js
@@ -1,49 +1,119 @@
+const userSelection = document.querySelectorAll(".buttons");
+const userDiv = document.querySelector(".user");
+const computerDiv = document.querySelector(".computer");
+const alertText = document.querySelector(".alert-text");
+const userScore = document.querySelector(".user-score").querySelector("p");
+const computerScore = document
+  .querySelector(".computer-score")
+  .querySelector("p");
+
+const fist = "./images/fist.png";
+const fistCpu = "./images/fist-cpu.png";
+const scissors = "./images/scissors.png";
+const scissorsCpu = "./images/scissors-cpu.png";
+const paper = "./images/paper.png";
+const paperCpu = "./images/paper-cpu.png";
+
+let results = [0, 0];
+
+userSelection.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const computerSele = computerSelection();
+
+    if (e.target.value === "rock") {
+      addUserImg(fist);
+    } else if (e.target.value === "paper") {
+      addUserImg(paper);
+    } else if (e.target.value === "scissors") {
+      addUserImg(scissors);
+    }
+
+    if (computerSele === "rock") {
+      addComputerImg(fistCpu);
+    } else if (computerSele === "paper") {
+      addComputerImg(paperCpu);
+    } else if (computerSele === "scissors") {
+      addComputerImg(scissorsCpu);
+    }
+
+    game(playRound, e.target.value, computerSele);
+  });
+});
+
+function addUserImg(src) {
+  const userImg = document.querySelector(".userImg");
+  userDiv.removeChild(userImg);
+  const img = document.createElement("img");
+  img.setAttribute("src", src);
+  img.classList.add("userImg");
+
+  userDiv.appendChild(img);
+}
+
+function addComputerImg(src) {
+  const computerImg = document.querySelector(".computerImg");
+  computerDiv.removeChild(computerImg);
+  const img = document.createElement("img");
+  img.setAttribute("src", src);
+  img.classList.add("computerImg");
+
+  computerDiv.appendChild(img);
+}
+
 function computerSelection() {
   const values = ["rock", "paper", "scissors"];
-
   return values[Math.floor(Math.random() * 3) + 1 - 1];
 }
 
-function playerSelection() {
-  const value = prompt("Ingrese algo gay!").toLocaleLowerCase();
-  return value;
-}
-
 function playRound(playerSelection, computerSelection) {
+  alertText.innerHTML = "";
+  const h3 = document.createElement("h3");
+
   if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You Win Rock beats Scissors!";
+    h3.textContent = "You Win Rock beats Scissors!";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You Win Paper beats rock!";
+    h3.textContent = "You Win Paper beats rock!";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You Win Scissors beats paper!";
+    h3.textContent = "You Win Scissors beats paper!";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    return "You Lose rock beats Scissors!";
+    h3.textContent = "You Lose rock beats Scissors!";
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    return "You Lose Paper beats rock!";
+    h3.textContent = "You Lose Paper beats rock!";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return "You Lose Scissors beats paper!";
+    h3.textContent = "You Lose Scissors beats paper!";
   } else {
-    return "It 's a tie!";
+    h3.textContent = "It 's a tie!";
+  }
+
+  alertText.appendChild(h3);
+  return h3.textContent;
+}
+
+function game(oneRound, playerSelection, computerSelection) {
+  const winOrLose = oneRound(playerSelection, computerSelection);
+
+  if (winOrLose.includes("Win")) {
+    results[0] = results[0] + 1;
+    userScore.textContent = results[0];
+  } else if (winOrLose.includes("Lose")) {
+    results[1] = results[1] + 1;
+    computerScore.textContent = results[1];
+  }
+
+  if (results[0] === 5) {
+    alert(`You WIN! ${results[0]} vs ${results[1]}`);
+    clearGame();
+  } else if (results[1] === 5) {
+    alert(`You Lose! ${results[0]} vs ${results[1]}`);
+    clearGame();
   }
 }
 
-function game(oneRound) {
-  const results = [0, 0];
-  for (let i = 0; i < 5; i++) {
-    const winOrLose = oneRound(playerSelection(), computerSelection());
-    console.log(winOrLose);
-    if (winOrLose.includes("Win")) {
-      results[0] = results[0] + 1;
-    } else if (winOrLose.includes("Lose")) {
-      results[1] = results[1] + 1;
-    }
-  }
-
-  if (results[0] > results[1]) {
-    return `You win! ${results[0]} vs ${results[1]}`;
-  } else if (results[0] < results[1]) {
-    return `You Lose! ${results[0]} vs ${results[1]}`;
-  } else return `Tie! ${results[0]} vs ${results[1]}`;
+function clearGame() {
+  alertText.innerHTML = "";
+  results = [0, 0];
+  computerScore.textContent = "0";
+  userScore.textContent = "0";
 }
 
-console.log(game(playRound));
+// console.log(game(playRound));
